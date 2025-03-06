e53269b212380753ef9b5ad71d19948b7ebefe18
suni <102209272+sunivibha@users.noreply.github.com>
2025-03-06 14:08:41 +0530

    deleted

--- main.tf
+++ main.tf
@@ -3,15 +3,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# ✅ Verify Email in AWS SES
-resource "aws_ses_email_identity" "email" {
-  email = "sunilkm88@gmail.com"
-}
 
-
-# ✅ Create IAM User
+# ✅ Create Iam user 
 resource "aws_iam_user" "user" {
   name = "servicenow-user"
   tags = {
     request_id = "SNOW12345"
-    requester  = "sunilkm88@gmail.com"
+    requester  = "sunilkm88@gmail.com" 
+  }
+}
